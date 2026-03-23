9cd06f0deecffa12503d7b6cf8fd8d5e3d46ec12
Julio Correa <julio.correa.rios@gmail.com>
2026-03-24 10:59:08 +1100

    Added memory management and ReAct logic to Pydantic Basemodel in State

--- app/state.py
+++ app/state.py
@@ -10,13 +10,25 @@ class AgentState(BaseModel):
 
     user_input: str
 
+     user_input: str
+
+    # --- NEW (ReAct logic implemented in this new branch) ---
+    thought: Optional[str] = None
+    action: Optional[str] = None
+    action_input: Optional[str] = None
+
     # Updating this comment due to implementing a smarter parsing logic
     needs_search: Optional[bool] = None
     search_query: Optional[str] = None
 
+    # Existing from baseline implementation
     search_results: List[SearchResult] = Field(default_factory=list)
-
     final_answer: Optional[str] = None
 
+    # --- MEMORY ---
+    intermediate_steps: List[str] = Field(default_factory=list)
+
     # Adding this one to track the iterations over topk results
+    # Loop control logic
     iteration: int = 0
+    max_iterations: int = 3
