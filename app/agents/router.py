78815840294447a5bd27dadbc3c7a590706049ad
Julio Correa <julio.correa.rios@gmail.com>
2026-03-24 11:22:12 +1100

    Change router funtion to implement ReAct iterations

--- app/agents/router.py
+++ app/agents/router.py
@@ -1,10 +1,20 @@
 from app.state import AgentState
 
 
-def route_after_planner(state: AgentState):
+# def route_after_planner(state: AgentState):
 
-    # This is what it needs to get done after getting and understanding the task in the planner node
-    if state.needs_search:
-        return "executor"
+#     # This is what it needs to get done after getting and understanding the task in the planner node
+#     if state.needs_search:
+#         return "executor"
 
-    return "responder"
+#     return "responder"
+
+def should_continue(state: AgentState):
+
+    if state.final_answer:
+        return "end"
+
+    if state.iteration >= state.max_iterations:
+        return "end"
+
+    return "continue"
